81ed297b2e16670ec9b2419601f342f688067295
Silvio Braendle <silvio@silviobraendle.ch>
2025-01-06 13:28:55 +0100

    Heal egui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use eframe::egui;
 use std::{sync::Arc, thread};
 
 mod app_service;
@@ -10,11 +11,47 @@ mod sound_engine;
 
 use crate::app_service::AppService;
 
-fn main() {
-    let app_service = Arc::new(AppService::new());
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
 
-    let app_service_listener = Arc::clone(&app_service);
-    thread::spawn(move || {
-        app_service_listener.listen();
-    });
+            Ok(Box::<UIApp>::default())
+        }),
+    )
+}
+
+struct UIApp {
+    app_service: Arc<AppService>,
+}
+
+impl Default for UIApp {
+    fn default() -> Self {
+        let app_service = Arc::new(AppService::new());
+
+        let app_service_listener = Arc::clone(&app_service);
+        thread::spawn(move || {
+            app_service_listener.listen();
+        });
+
+        Self {
+            app_service: app_service,
+        }
+    }
+}
+
+impl eframe::App for UIApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.label("Hello");
+        });
+    }
 }
