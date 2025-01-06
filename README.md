2c148ea6d5c01495686f9b748c4e23783ee8b265
cediackermann <ackermanncedi@gmail.com>
2025-01-06 13:16:43 +0100

    Add Frontend file and update README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # syncaud
+
+## Featurelist
+- Interactive UI
+- Play sounds synchronously across multiple devices
+- Upload mp3 files to play
--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ fn main() -> eframe::Result {
     };
 
     eframe::run_native(
-        "My egui App",
+        "Syncaud",
         options,
         Box::new(|cc| {
             // This gives us image support:
@@ -44,24 +44,10 @@ fn main() -> eframe::Result {
 }
 
 struct UIApp {
-    name: String,
-    age: u32,
+    username: String,
+    ip_address: String,
 }
 
 impl eframe::App for UIApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Increment").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
-        });
-    }
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {}
 }
